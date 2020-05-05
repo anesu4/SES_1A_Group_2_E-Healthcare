16a7e5172c891416a5dd09a494c53199ba48ec6f
Anesu <anesu4@yahoo.com.au>
2020-05-05 15:06:47 +0800

    New app

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Index Home Page
 Route::get('/', function () {
-    return view('index');
-});
-
-//User Authentication
-Route::get('/login', 'auth\LoginController@index')->name('login');
-Route::post('/login', 'auth\LoginController@login')->name('login');
-Route::get('/logout', 'auth\LoginController@logout')->name('logout');
-Route::post('/logout', 'auth\LoginController@logout')->name('logout');
-Route::get('/register/{pid?}', 'auth\RegisterController@index')->name('register');
-Route::post('/register/{pid?}', 'auth\RegisterController@register')->name('register');
-
-//Error Page
-Route::any('{all}', function(){
-    return view('errors.404');
+    return view('welcome');
 });
