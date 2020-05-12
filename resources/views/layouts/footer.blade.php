a35a1ca1b83c016233e783bf844a04a90ec611a7
Anesu <anesu4@yahoo.com.au>
2020-05-12 14:25:05 +0800

    Registeration and Login Pages on their own

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <p>
+            Footer Info Here:
+        </p>
+    </body>
+</html>
---
+++ storage/framework/views/33cb35c96728944c4f38f9cb3e390b5fe778385f.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <p>
+            Footer Info Here:
+        </p>
+    </body>
+</html>
+<?php /**PATH C:\Users\Anesu\OneDrive\UTS\Semester 1\Software Studio\E-Healthcare\SES_1A_Group_2_E-Healthcare\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
