6f840aa8f6b531f42a34c7bf47dd5ea023fef237
Paul M <13634151@student.uts.edu.au>
2020-05-26 16:05:26 +1000

    message display update

--- storage/framework/views/4286428a48b0e8d0501f87e697bc85f9d053ed4f.php
+++ storage/framework/views/4286428a48b0e8d0501f87e697bc85f9d053ed4f.php
@@ -7,11 +7,11 @@
             <div class="row">
                 <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 ">
                     <h3>Chats</h3>
-                    <div class="inbox_chat ">
-                        <button type="button" class="btn btn-block w-75" >
-                            <span class="" style="max-height: 20px;">
-                            <h5 class="text-left">Dr Name</h5>
-                            <!-- <p class="text-left ">Hey, may I book an appointment for 12:00pm?</p> -->
+                    <div class="inbox-chat ">
+                        <button type="button" class="btn btn-block" style="height:75px; overflow:hidden;">
+                            <span class="">
+                                <h5 class="text-left">Dr Name</h5>
+                                <p class="text-left ">Hey, may I book an appointment for 12:00pm?</p>
                             </span>
                         </button>
                     </div>
@@ -21,14 +21,19 @@
                         <h3>Dr. Example name</h3>
                     </div>
                     <div class="chat-history">
-
+                        <div class="inbox-message in">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, tempora?</p>
+                        </div>
+                        <div class="inbox-message out">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, tempora?</p>
+                        </div>
                     </div>
                     <div class="container-fluid">
                         <form action="">
                             <div class="row">
                                 <div class="col-xs-8" >
                                     <div class="text-center w-100"><input type="text" class="form-control" placeholder="enter message" >
-            
+
                                     </div>
                                 </div>
                                 <div class="col-xs-4" >
